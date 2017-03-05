2a0e3c3722a0d3792841d1742e9c7f77037b7ee8
Harrison Hocker <hjhocker@gmail.com>
2017-03-05 12:47:26 -0600

    skill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SkillComponent } from './skill/skill.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +21,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
