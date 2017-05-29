f8a32660f323ac645673c31be56ad9adeed464dd
Harrison Hocker <hjhocker@gmail.com>
2017-05-29 08:52:59 -0500

    page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { EducationComponent } from './education/education.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { AboutComponent } from './about/about.component';
 import { ResearchComponent } from './research/research.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {path: 'experience', component: ExperienceComponent},
   {path: 'education', component: EducationComponent},
   {path: 'previousresearch', component: ResearchComponent},
-  {path: 'ots', component: AboutComponent}
+  {path: 'ots', component: AboutComponent},
+  {path: '404', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -23,7 +25,8 @@ const appRoutes: Routes = [
     EducationComponent,
     ExperienceComponent,
     AboutComponent,
-    ResearchComponent
+    ResearchComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
