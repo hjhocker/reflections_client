210806d38635378d6778529de2f483407cb63d0d
Harrison Hocker <hjhocker@gmail.com>
2017-05-28 22:20:25 -0500

    basic navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'app works!';
+
+  clicked(event) {
+    console.log("clicked on about this site");
+  }
+
 }
