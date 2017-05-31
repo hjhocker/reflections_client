98b66a5cb1f76f191552586616343f0a5caed417
Harrison <hjhocker@gmail.com>
2017-05-31 07:56:22 -0500

    get skills from the API (#24)
    * get skills from the API
    
    * ignore test

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Skill } from './skill'
+
+import { environment } from '../../environments/environment';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-experience',
@@ -6,10 +15,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
+  skills: Skill[];
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-  ngOnInit() {
+  ngOnInit() {this.getSkills()}
+
+  getSkills() {
+    this.http.get(environment.apiUrl + '/api/skills')
+                .map(this.extractSkills)
+                .catch(this.handleError)
+                .subscribe((data) => this.skills = data);
+  }
+
+  private extractSkills(res: Response) {
+    return res.json();
+  }
+
+  private handleError (error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
 }
