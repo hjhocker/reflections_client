faf278e6bced1e4895cdde1c72ee51ce894333f9
Harrison Hocker <hjhocker@gmail.com>
2017-06-01 22:19:29 -0500

    more content

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  devCycle: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.devCycle = "/assets/images/dev_cycle.png"
   }
 
 }
