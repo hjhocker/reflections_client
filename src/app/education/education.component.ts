8a1406919db84634e85c78a426a4c022bdc488d8
Harrison <hjhocker@gmail.com>
2017-06-01 22:24:01 -0500

    education, development cycle content (#27)
    * more content
    
    * update
    
    * sonar

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+  utLogo: string;
+  uthscLogo: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.utLogo = "/assets/images/GSBS_min_5c_cobrand_lrg.png"
+    this.uthscLogo = "/assets/images/Texas_Longhorn_logo.svg.png"
   }
 
 }
